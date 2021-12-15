e43de6836cc61e13abbd2cfbf0804454bcaac955
dongmin <ddmkim94@gmail.com>
2021-12-16 02:09:12 +0900

    프론트 컨트롤러 - Model 추가(v3)

--- src/main/java/hello/servlet/domain/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/domain/web/frontcontroller/MyView.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
 
@@ -17,5 +18,16 @@ public class MyView {
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 필요한 데이터를 전부 담아서 forward! -> view에서 getAttribute로 꺼내서 사용!
+        modelToRequestAttribute(model, request);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach((key, value) -> request.setAttribute(key, value));
     }
 }
