437df6f42e2062c9ba0a7c3975105837c20503ed
dongmin <ddmkim94@gmail.com>
2021-12-17 13:29:34 +0900

    프론트 컨트롤러 - ModelView 제거(v4)

---
+++ src/main/java/hello/servlet/domain/web/frontcontroller/v4/controller/MemberFormControllerV4.java
@@ -0,0 +1,19 @@
+package hello.servlet.domain.web.frontcontroller.v4.controller;
+
+import hello.servlet.domain.web.frontcontroller.v4.ControllerV4;
+
+import java.util.Map;
+
+public class MemberFormControllerV4 implements ControllerV4 {
+
+    /**
+     * @param paramMap
+     * @param model
+     * @return viewName
+     */
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        return "new-form";
+    }
+}
