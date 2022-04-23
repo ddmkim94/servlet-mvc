ae3d03df1a794a9cd9dba216c2e5feebfbb07a31
ddmkim <ddmkim94@gmail.com>
2022-04-23 15:21:24 +0900

    회원 도메인, 저장소 생성 & 테스트

--- src/main/java/hello/servlet/domain/member/MemberRepository.java
+++ src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -5,18 +5,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
+// @NoArgsConstructor(access = AccessLevel.PRIVATE) 기본 생성자를 private 레벨로 생성
 public class MemberRepository {
 
     private static final Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
 
+    // 저장소 객체는 싱글톤으로 관리
     private static final MemberRepository instance = new MemberRepository();
+
+    // 저장소 객체는 getInstance() 메서드로만 받을 수 있음!
     public static MemberRepository getInstance() {
         return instance;
     }
 
-    // 싱글톤을 위해 private 생성자 정의
-    private MemberRepository() {}
+    // 외부에서 맘대로 생성할 수 없도록 private 레벨로 생성
+    private MemberRepository(){}
 
     public Member save(Member member) {
         member.setId(++sequence);
@@ -35,6 +40,4 @@ public class MemberRepository {
     public void clearStore() {
         store.clear();
     }
-
-
 }
--- src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -6,14 +6,16 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MemberRepositoryTest {
 
-    private final MemberRepository memberRepository = MemberRepository.getInstance();
+    MemberRepository memberRepository = MemberRepository.getInstance();
 
-    // 각 테스트 종료 후 실행되는 메서드
+    /**
+     * 테스트가 종료되고 실행되는 메서드!
+     * 정확한 테스트를 위해 저장소를 말끔히 비워주는 역할
+      */
     @AfterEach
     void afterEach() {
         memberRepository.clearStore();
@@ -21,23 +23,22 @@ class MemberRepositoryTest {
 
     @Test
     void save() {
-        Member member1 = new Member("은빈", 31);
-        Member saveMember = memberRepository.save(member1);
-        Member findMember = memberRepository.findById(saveMember.getId());
+        Member member = new Member("연서", 36);
+        Member saveMember = memberRepository.save(member);
 
+        Member findMember = memberRepository.findById(saveMember.getId());
         assertThat(findMember).isEqualTo(saveMember);
     }
 
     @Test
     void findAll() {
-        Member member1 = new Member("은빈", 31);
-        Member member2 = new Member("동민", 28);
+        Member member1 = new Member("연서", 36);
+        Member member2 = new Member("동민", 29);
 
         memberRepository.save(member1);
         memberRepository.save(member2);
 
         List<Member> members = memberRepository.findAll();
-
         assertThat(members.size()).isEqualTo(2);
         assertThat(members).contains(member1, member2);
     }
